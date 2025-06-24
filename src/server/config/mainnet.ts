c17bc0e143de3e763170dd14e7c0704a92017bf4
arkff202310 <109507401+arkff202310@users.noreply.github.com>
2025-06-24 19:23:11 +0800

    chore: mainnet config (#14)

--- src/server/config/mainnet.ts
+++ src/server/config/mainnet.ts
@@ -4,11 +4,13 @@ import { optionalEnv, requiredEnv } from './helper';
 const config: Config = {
   networkId: 'mainnet',
   nodeUrl: optionalEnv('NEAR_RPC') || 'https://near.lava.build',
-  subgraphApiUrl: optionalEnv('SUBGRAPH_API_URL') || '', // TODO
+  subgraphApiUrl:
+    optionalEnv('SUBGRAPH_API_URL') ||
+    'https://api.studio.thegraph.com/query/76854/near-validator-voting-reduce-inflation/v0.0.1',
   subgraphAPiKey: optionalEnv('SUBGRAPH_API_KEY'),
   nearBlocksApiUrl: 'https://api.nearblocks.io/v2',
   poolDetailContractId: 'pool-details.near',
-  votingContractId: '', // TODO
+  votingContractId: 'reduce-inflation.near',
   port: Number(requiredEnv('PORT')),
 };
 
