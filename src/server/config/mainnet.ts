1e8fad40099ae8b3b00012f1bb049799837f7e4c
arkff <109507401+arkff202310@users.noreply.github.com>
2025-06-24 17:48:06 +0800

    chore: mainnet config

--- src/server/config/mainnet.ts
+++ src/server/config/mainnet.ts
@@ -6,7 +6,7 @@ const config: Config = {
   nodeUrl: optionalEnv('NEAR_RPC') || 'https://near.lava.build',
   subgraphApiUrl:
     optionalEnv('SUBGRAPH_API_URL') ||
-    'https://api.studio.thegraph.com/query/76854/near-validator-voting-reduce-inflation/v0.0.1', // TODO
+    'https://api.studio.thegraph.com/query/76854/near-validator-voting-reduce-inflation/v0.0.1',
   subgraphAPiKey: optionalEnv('SUBGRAPH_API_KEY'),
   nearBlocksApiUrl: 'https://api.nearblocks.io/v2',
   poolDetailContractId: 'pool-details.near',
